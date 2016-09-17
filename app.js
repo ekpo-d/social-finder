a24208b213da88e1932d41afecc545e5170cbe0c
Ekpo David <ekpodave@rocketmail.com>
2016-09-17 06:48:55 +0100

    update

---
+++ app.js
@@ -0,0 +1,11 @@
+var http = require('http')
+var router = require ('./router/router.js')
+
+function mainProcess(req, res){
+  res.writeHead(200, {'Content-Type' : 'text/plain'})
+  router(req, res)
+  res.end()
+}
+
+http.createServer(mainProcess).listen(process.env.PORT || 8080)
+console.log('server is running')
